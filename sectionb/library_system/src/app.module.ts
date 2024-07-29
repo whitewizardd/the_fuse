fb1210666abf4b9221082b9170da2d18d17df194
whitewizardd <leumasre@gmail.com>
2024-07-29 12:13:35 +0100

    library system

--- sectionb/library_system/src/app.module.ts
+++ sectionb/library_system/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule, AuthorModule],
   controllers: [AppController],
   providers: [AppService],
 })
